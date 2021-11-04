f80cca7cdf0f8c693408420c130a4674ade5ac5d
Vinicius Sa <viniciusjavs@gmail.com>
2021-11-03 22:31:07 -0300

    Exercises: 1.2 - course information, step 2
    Refactor the Content component so that it renders three Part components.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,17 +23,19 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <>
+    <div>
+      <Part content={props.content[0]}/>
+      <Part content={props.content[1]}/>
+      <Part content={props.content[2]}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
     <p>
-      {props.content[0].part} {props.content[0].exercises}
+      {props.content.part} {props.content.exercises}
     </p>
-    <p>
-      {props.content[1].part} {props.content[1].exercises}
-    </p>
-    <p>
-      {props.content[2].part} {props.content[2].exercises}
-    </p>
-    </>
   )
 }
 
