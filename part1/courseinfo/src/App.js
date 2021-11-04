dac30bc38da324230a32c8a4b4c14f7903b19540
Vinicius Sa <viniciusjavs@gmail.com>
2021-11-03 21:34:10 -0300

    Exercises: 1.1 - course information, step 1
    Refactor the code so that it consists of three new components: Header, Content,
    and Total.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,27 +2,44 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const content = [{part: 'Fundamentals of React', exercises: 10},
+                   {part: 'Using props to pass data', exercises: 7},
+                   {part: 'State of a component', exercises: 10}]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content content={content}/>
+      <Total total={content.reduce((acc, content) => acc + content.exercises, 0)}/>
     </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+    <p>
+      {props.content[0].part} {props.content[0].exercises}
+    </p>
+    <p>
+      {props.content[1].part} {props.content[1].exercises}
+    </p>
+    <p>
+      {props.content[2].part} {props.content[2].exercises}
+    </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
   )
 }
 
