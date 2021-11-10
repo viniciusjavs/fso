59eb532a68869709d7eaa423295fb9c0270136c1
Vinicius Sa <viniciusjavs@gmail.com>
2021-11-10 16:05:56 -0300

    Exercises: 2.5 - separate module
    Declare the Course component as a separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -47,40 +48,6 @@ const App = () => {
   ]
 
   return <Course courses={courses} />
-}
-
-const Course = ({courses}) => {
-  return (
-    <>
-      {courses.map(({id, name, parts}) =>
-        <div key={id}>
-          <Header name={name} />
-          <Content parts={parts} />
-          <Total parts={parts} />
-        </div>
-      )}
-    </>
-  )
-}
-
-const Header = ({ name }) => <h1>{name}</h1>
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map(({id, name, exercises}) => <Part key={id} name={name} exercises={exercises} />)}
-    </div>
-  )
-}
-
-const Part = ({name, exercises}) => <p> {name} {exercises} </p>
-
-const Total = ({parts}) => {
-  return (
-    <p>
-      <b>Total of {parts.reduce((acc, {exercises}) => acc + exercises, 0)} exercises</b>
-    </p>
-  )
 }
 
 export default App
