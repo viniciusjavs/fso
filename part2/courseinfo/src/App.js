2dac0212fcc97f2766e239cb685d56ec386192a6
Vinicius Sa <viniciusjavs@gmail.com>
2021-11-10 15:55:19 -0300

    Exercises: 2.4 - Course information - step 9
    Allow an arbitrary number of courses

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,37 +1,64 @@
 import React from 'react'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        id: 1,
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        id: 2,
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        id: 3,
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          id: 1,
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          id: 2,
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          id: 3,
+          name: 'State of a component',
+          exercises: 14
+        },
+        {
+          id: 4,
+          name: 'Redux',
+          exercises: 11
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        {
+          id: 1,
+          name: 'Routing',
+          exercises: 3
+        },
+        {
+          id: 2,
+          name: 'Middlewares',
+          exercises: 7
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return <Course courses={courses} />
 }
 
-const Course = ({course: {name, parts}}) => {
+const Course = ({courses}) => {
   return (
     <>
-      <Header name={name} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      {courses.map(({id, name, parts}) =>
+        <div key={id}>
+          <Header name={name} />
+          <Content parts={parts} />
+          <Total parts={parts} />
+        </div>
+      )}
     </>
   )
 }
