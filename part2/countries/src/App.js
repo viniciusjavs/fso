185ab904c9d293e8b4d01061ab61e94dcfb85bc6
Vinicius Sa <viniciusjavs@gmail.com>
2021-11-18 23:22:41 -0300

    Exercises: 2.14 - Data for countries - step 3
    Add the weather report for the capital of the selected country

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -56,12 +56,46 @@ const Country = ({ country: {name, capital, population, languages, flags} }) => 
       <h1>{name}</h1>
       <p>capital {capital}</p>
       <p>population {population}</p>
-      <h2>languages</h2>
+      <h2>Spoken languages</h2>
       <ul>
         {languages.map(({ name, iso639_2 }) => <li key={iso639_2}>{name}</li>)}
       </ul>
       <img src={flags.png} alt={"flag of " + name}/>
+      <Weather city={capital} />
     </div>
+  )
+}
+
+const Weather = ({ city }) => {
+  const [temp, setTemp] = useState('')
+  const [weather_icons, setWeatherIcons] = useState([])
+  const [wind, setWind] = useState('')
+
+  const toMph = (kph) => Math.round(kph / 1.609344)
+
+  useEffect(() => {
+    axios
+      .get('http://api.weatherstack.com/current', {
+        params: {
+          access_key: process.env.REACT_APP_API_KEY,
+          query: city
+        }
+      })
+      .then(response => {
+        const { current: {temperature, weather_icons, wind_speed, wind_dir} } = response.data
+        setTemp(`${temperature} Celsius`)
+        setWeatherIcons(weather_icons)
+        setWind(`${toMph(wind_speed)} mph direction ${wind_dir}`)
+      })
+  }, [city])
+
+  return (
+    <>
+      <h2>Weather in {city}</h2>
+      <p><b>temperature:</b> {temp}</p>
+      {weather_icons.map(icon => <img key={icon} src={icon} alt={`weather icon of ${city}`}/>)}
+      <p><b>wind:</b> {wind}</p>
+    </>
   )
 }
 
