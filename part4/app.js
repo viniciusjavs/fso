758d08c8b2148962b6d1d2faeb3e064446d5c76f
Vinicius Sa <viniciusjavs@gmail.com>
2021-12-03 15:34:44 -0300

    Blog List, step 2
    Refactor the application into separate modules

---
+++ part4/app.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const cors = require('cors')
+const express = require('express')
+const blogsRouter = require('./controllers/blog')
+const logger = require('./utils/logger')
+
+const app = express()
+
+logger.info(`Connecting to ${config.MONGODB_URI}`)
+mongoose
+  .connect(config.MONGODB_URI.replace('MONGODB_PASS', config.MONGODB_PASS))
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error(`Error connecting to MongoDB: ${error.message}`)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
---
+++ part4/utils/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_PASS = process.env.MONGODB_PASS
+const MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = {
+    PORT,
+    MONGODB_PASS,
+    MONGODB_URI
+}
