c9359653e1b0149b80bd490a6a72167680251217
Vinicius Sa <viniciusjavs@gmail.com>
2021-12-03 23:49:32 -0300

    Exercises: 4.5 - helper functions and unit tests - step 3
    Define the favoriteBlog function that receives a list of blogs and
    returns the one with the most likes

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,6 +1,11 @@
-const totalLikes  = blogs => blogs
-                                .reduce((acc, { likes }) => acc + likes, 0)
+const totalLikes = blogs => blogs
+  .reduce((acc, { likes }) => acc + likes, 0)
+
+const favoriteBlog = blogs => blogs
+  .map(({ title, author, likes }) => ({ title, author, likes }))
+  .reduce((acc, blog) => blog.likes < acc.likes ? acc : blog, {})
 
 module.exports = {
-    totalLikes
+  totalLikes,
+  favoriteBlog
 }
