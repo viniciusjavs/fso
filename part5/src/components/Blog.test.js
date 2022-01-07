6e3ae794c4892e43766cc8d3c6a78d1f38c32bdb
Vinicius Sa <viniciusjavs@gmail.com>
2022-01-07 00:01:10 -0300

    Exercises: 5.14 - Blog list tests  - step 2
    Test rendering a blog
    
    Show url and likes when view button is clicked

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,32 +1,50 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('shows title and author but hides url and likes', () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'C++11: The Rule of the Big Five',
     author: 'Cline, Lomow, Girou',
     url: 'https://www.feabhas.com/sites/default/files/2016-06/Rule%20of%20the%20Big%20Five.pdf',
     likes: 42
   }
+  let mockHandler
+  let component
 
-  const mockHandler = jest.fn()
+  beforeEach(() => {
+    mockHandler = jest.fn()
+    component = render(
+      <Blog blog={blog} handleUpdate={mockHandler} handleRemove={mockHandler} />
+    )
+  })
 
-  const component = render(
-    <Blog blog={blog} handleUpdate={mockHandler} handleRemove={mockHandler} />
-  )
+  test('shows title and author but hides url and likes', () => {
+    expect(component.container).toHaveTextContent(blog.title)
+    expect(component.container).toHaveTextContent(blog.author)
+    expect(component.container).toHaveTextContent(blog.url)
+    expect(component.container).toHaveTextContent(blog.likes)
 
-  expect(component.container).toHaveTextContent(blog.title)
-  expect(component.container).toHaveTextContent(blog.author)
-  expect(component.container).toHaveTextContent(blog.url)
-  expect(component.container).toHaveTextContent(blog.likes)
+    expect(component.container.querySelector('.basic-blog')).not.toHaveStyle(
+      'display: none'
+    )
 
-  expect(component.container.querySelector('.basic-blog')).not.toHaveStyle(
-    'display: none'
-  )
+    expect(component.container.querySelector('.full-blog')).toHaveStyle(
+      'display: none'
+    )
+  })
 
-  expect(component.container.querySelector('.full-blog')).toHaveStyle(
-    'display: none'
-  )
+  test('clicking view button displays url and likes', () => {
+    const button = component.container.querySelector('button')
+    fireEvent.click(button)
+
+    expect(component.container.querySelector('.basic-blog')).toHaveStyle(
+      'display: none'
+    )
+
+    expect(component.container.querySelector('.full-blog')).not.toHaveStyle(
+      'display: none'
+    )
+  })
 })
