26063332047b696c51566078d8529620705ad981
Vinicius Sa <viniciusjavs@gmail.com>
2022-01-20 23:13:52 -0300

    Exercises: 6.13 - Anecdotes and the backend - step 1
    Fetch the anecdotes from the backend.

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(async () => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(initializeAnecdotes(anecdotes))
+  })
+
   return (
     <div>
       <h2>Anecdotes</h2>
