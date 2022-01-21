40722c3bababf2424d105bf1ffb7233c760361f1
Vinicius Sa <viniciusjavs@gmail.com>
2022-01-21 19:40:44 -0300

    Exercises: 6.15 - Anecdotes and the backend - step 3
    Initialize the redux store asynchronously using the redux thunk lib

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 const anecdoteReducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
@@ -29,10 +31,13 @@ export const createAnecdote = anecdote => {
   }
 }
 
-export const initializeAnecdotes = anecdotes => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
   }
 }
 
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,14 +1,20 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
 import { composeWithDevTools } from '@redux-devtools/extension'
+import thunk from 'redux-thunk'
 
-const store = createStore(combineReducers({
-    anecdotes: anecdoteReducer,
-    notification: notificationReducer,
-    filter: filterReducer
-}), composeWithDevTools)
+const store = createStore(
+    combineReducers({
+        anecdotes: anecdoteReducer,
+        notification: notificationReducer,
+        filter: filterReducer
+    }),
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+)
 
 console.log(store.getState())
 
