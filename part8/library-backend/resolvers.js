a4a690664f52716482155948abdde4b445828e7b
Vinicius Sa <viniciusjavs@gmail.com>
2022-03-08 00:26:44 -0300

    Exercises: 8.26 - Solve n+1 problem for allAuthors query

--- part8/library-backend/resolvers.js
+++ part8/library-backend/resolvers.js
@@ -9,93 +9,105 @@ const Token = require('./models/token')
 const jwt = require('jsonwebtoken')
 
 const resolvers = {
-    Author: {
-      bookCount: async (root) => Book.countDocuments({ author: root.id })
-    },
-    Query: {
-      bookCount: async () => Book.collection.countDocuments(),
-      authorCount: async () => Author.collection.countDocuments(),
-      allBooks: async (_root, { author, genre }) => {
-        const query = {}
-        if (author) {
-          const author_id = await Author.find({name: author}, 'id')
-          query.author = author_id
-        }
-        if (genre) {
-          query.genres = genre
-        }
-        return Book.find(query).populate('author')
-      },
-      allAuthors: async () => await Author.find({}),
-      me: async (_root, _args, { user } ) => {
-        if (!user) {
-          throw new AuthenticationError()
-        }
-        return User.findOne(user)
+  Author: {
+    bookCount: async (root) => root.bookCount || Book.countDocuments({ author: root.id })
+  },
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (_root, { author, genre }) => {
+      const query = {}
+      if (author) {
+        const author_id = await Author.find({name: author}, 'id')
+        query.author = author_id
       }
+      if (genre) {
+        query.genres = genre
+      }
+      return Book.find(query).populate('author')
     },
-    Mutation: {
-      addBook: async (_root, args, { user }) => {
-        if (!user) {
-          throw new AuthenticationError()
+    allAuthors: async () => {
+      const authors = await Author.find({})
+      const books = await Book.find({}, 'author')
+      authors.forEach((author) => {
+        author.bookCount = books.reduce((acc, b) => {
+          return b.author.toString() === author.id
+            ? acc + 1
+            : acc
         }
-        let author = await Author.findOne({ name: args.author })
-        if (!author) {
-          try {
-            author = await Author.create({ name: args.author })
-          }
-          catch (error) {
-            throw new UserInputError(error.message, {
-              invalidArgs: args.author
-            })
-          }
-        }
-
+        , 0)
+      })
+      return authors
+    },
+    me: async (_root, _args, { user } ) => {
+      if (!user) {
+        throw new AuthenticationError()
+      }
+      return User.findOne(user)
+    }
+  },
+  Mutation: {
+    addBook: async (_root, args, { user }) => {
+      if (!user) {
+        throw new AuthenticationError()
+      }
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
         try {
-          const book = await Book.create({ ...args, author: author._id })
-          await book.populate('author')
-          pubsub.publish('BOOK_ADDED', { bookAdded: book })
-          return book
+          author = await Author.create({ name: args.author })
         }
-        catch(error) {
+        catch (error) {
           throw new UserInputError(error.message, {
-            invalidArgs: { ...args, author: author._id }
+            invalidArgs: args.author
           })
         }
-      },
-      editAuthor: async (_root, { name, setBornTo }, { user }) => {
-        if(!user) {
-          throw new AuthenticationError()
-        }
-        return Author.findOneAndUpdate({ name }, { born: setBornTo }, {
-          returnOriginal: false
-        })
-      },
-      login: async (_root, { username, password }) => {
-        const user = await User.findOne({ username })
-        if (!user || password !== 'secret') {
-          throw new UserInputError('wrong credentials')
-        }
-        const token = {
-          value: jwt.sign(
-            {
-              username: user.username,
-              id: user._id
-            },
-            config.JWT_SECRET
-          )
-        }
-        await Token.create({ token: token.value, userId: user._id })
-        return token
-      },
-      createUser: async (_root, args) => User.create({ ...args })
-    },
-    Subscription: {
-      bookAdded: {
-        subscribe: () => pubsub.asyncIterator(['BOOK_ADDED'])
       }
+
+      try {
+        const book = await Book.create({ ...args, author: author._id })
+        await book.populate('author')
+        pubsub.publish('BOOK_ADDED', { bookAdded: book })
+        return book
+      }
+      catch(error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: { ...args, author: author._id }
+        })
+      }
+    },
+    editAuthor: async (_root, { name, setBornTo }, { user }) => {
+      if(!user) {
+        throw new AuthenticationError()
+      }
+      return Author.findOneAndUpdate({ name }, { born: setBornTo }, {
+        returnOriginal: false
+      })
+    },
+    login: async (_root, { username, password }) => {
+      const user = await User.findOne({ username })
+      if (!user || password !== 'secret') {
+        throw new UserInputError('wrong credentials')
+      }
+      const token = {
+        value: jwt.sign(
+          {
+            username: user.username,
+            id: user._id
+          },
+          config.JWT_SECRET
+        )
+      }
+      await Token.create({ token: token.value, userId: user._id })
+      return token
+    },
+    createUser: async (_root, args) => User.create({ ...args })
+  },
+  Subscription: {
+    bookAdded: {
+      subscribe: () => pubsub.asyncIterator(['BOOK_ADDED'])
     }
   }
+}
 
-  module.exports = resolvers
+module.exports = resolvers
   
