125d6d9c33512c31ae6606517baa196a1ed965b9
Vinicius Sa <viniciusjavs@gmail.com>
2022-03-24 12:06:09 -0300

    Task 13.22: Mark a blog in the reading list as read

--- part13/controllers/reading_lists.js
+++ part13/controllers/reading_lists.js
@@ -1,9 +1,28 @@
 const readingListsRouter = require('express').Router()
 const { ReadingLists } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 readingListsRouter.post('/', async (req, res) => {
     const listEntry = await ReadingLists.create(req.body)
     res.json(listEntry)
+})
+
+readingListsRouter.put('/:id', tokenExtractor, async (req, res) => {
+    const listEntry = await ReadingLists.findOne({
+        where: {
+            id: req.params.id,
+            userId: req.decodedToken.id
+        }
+    })
+    if (listEntry) {
+        if (req.body.state) {
+            listEntry.state = req.body.state
+        }
+        await listEntry.save()
+        res.json(listEntry)
+    } else {
+        res.status(404).end()
+    }
 })
 
 module.exports = readingListsRouter
